cb99b8eec2eec16743ab488488e4bcb5323c8619
ISR-web-dev <137366576+ISR-web-dev@users.noreply.github.com>
2023-11-26 04:07:19 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+let testimonialCount = 0;
+
+const dataTestimonial = [
+    {
+        name: 'DA EDITS',
+        testimonials: '"He is really cooperative and he excel in getting the job done perfectly on time."' 
+    },
+    {
+        name: 'JOSHI ADS',
+        testimonials: '"IDK"' 
+    }
+]
+
+function changeNameTo(name) {
+    document.querySelector('.testi-name').innerHTML = name;
+} 
+function changeTestimonialTo(testimonial) {
+    document.querySelector('.testimonial').innerHTML = testimonial;
+}
+
+document.querySelector('.right').addEventListener('click', () => {
+    if (testimonialCount < 1) {
+        testimonialCount++;
+    } else {
+        testimonialCount = 0;
+    }
+    changeNameTo(dataTestimonial[testimonialCount].name);
+    changeTestimonialTo(dataTestimonial[testimonialCount].testimonials);
+});
+document.querySelector('.left').addEventListener('click', () => {
+    if (testimonialCount > 0) {
+        testimonialCount--;
+    } else {
+        testimonialCount = 1;
+    }
+    changeNameTo(dataTestimonial[testimonialCount].name);
+    changeTestimonialTo(dataTestimonial[testimonialCount].testimonials);
+});
+
